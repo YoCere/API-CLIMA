86e419a7cd98ba33b17af32d79a0db4ed6e26fb1
YoCere <jcerezorios@gmail.com>
2024-09-05 12:54:48 -0400

    Funcion de seleccionar departamento terminada

--- app.js
+++ app.js
@@ -1,86 +1,66 @@
-window.addEventListener('load', ()=> {
-    let lon, lat
+window.addEventListener('load', () => {
+    let key = 'f52d491af0f3cd0a2686235c7d04aacf';
 
-    let temperaturaValor = document.getElementById('temperatura-valor')  
-    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
-    let ubicacion = document.getElementById('ubicacion')  
-    let iconoAnimado = document.getElementById('icono-animado') 
-    let vientoVelocidad = document.getElementById('viento-velocidad') 
+    // Elementos HTML
+    let temperaturaValor = document.getElementById('temperatura-valor');
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
+    let ubicacion = document.getElementById('ubicacion');
+    let iconoAnimado = document.getElementById('icono-animado');
+    let vientoVelocidad = document.getElementById('viento-velocidad');
+    let selectDepartamento = document.getElementById('departamento');
 
-    if(navigator.geolocation){
-       navigator.geolocation.getCurrentPosition( posicion => {
-           lon = posicion.coords.longitude
-           lat = posicion.coords.latitude
-            //Lon= Longitud Lat=
-            let key ='f52d491af0f3cd0a2686235c7d04aacf'
+    // Función para actualizar el clima
+    function actualizarClima(ciudad) {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},BO&lang=es&units=metric&appid=${key}`;
 
-            const url =  `https://api.openweathermap.org/data/2.5/weather?q=Madrid&lang=es&units=metric&appid=${key}`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let temp = Math.round(data.main.temp);
+                temperaturaValor.textContent = `${temp} °C`;
 
-           //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f52d491af0f3cd0a2686235c7d04aacf`
+                let desc = data.weather[0].description;
+                temperaturaDescripcion.textContent = desc.toUpperCase();
+                ubicacion.textContent = data.name;
 
-           console.log(url)
-           fetch(url)
-            .then( response => { return response.json()})
-            .then( data => {
-                
-                let temp = Math.round(data.main.temp)
-                
-  
+                vientoVelocidad.textContent = `${data.wind.speed} m/s`;
 
-                temperaturaValor.textContent = `${temp} ° C`
-                //console.log(data.weather[0].description)
-                let desc = data.weather[0].description
-                temperaturaDescripcion.textContent = desc.toUpperCase()
-                ubicacion.textContent = data.name
-                
-                vientoVelocidad.textContent = `${data.wind.speed} m/s`
-                
-                //para iconos estáticos
-                //const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`                     
-                //icono.src = urlIcon
-                //console.log(data.weather[0].icon)
-
-                //para iconos dinámicos
-                console.log(data.weather[0].main)
-                  switch (data.weather[0].main) {
-                      case 'Thunderstorm':
-                        iconoAnimado.src='animated/thunder.svg'
-                        console.log('TORMENTA');
+                // Iconos dinámicos basados en el clima
+                switch (data.weather[0].main) {
+                    case 'Thunderstorm':
+                        iconoAnimado.src = 'animated/thunder.svg';
                         break;
-                      case 'Drizzle':
-                        iconoAnimado.src='animated/rainy-2.svg'
-                        console.log('LLOVIZNA');
+                    case 'Drizzle':
+                        iconoAnimado.src = 'animated/rainy-2.svg';
                         break;
-                      case 'Rain':
-                        iconoAnimado.src='animated/rainy-7.svg'
-                        console.log('LLUVIA');
+                    case 'Rain':
+                        iconoAnimado.src = 'animated/rainy-7.svg';
                         break;
-                      case 'Snow':
-                        iconoAnimado.src='animated/snowy-6.svg'
-                          console.log('NIEVE');
-                        break;                        
-                      case 'Clear':
-                          iconoAnimado.src='animated/day.svg'
-                          console.log('LIMPIO');
+                    case 'Snow':
+                        iconoAnimado.src = 'animated/snowy-6.svg';
                         break;
-                      case 'Atmosphere':
-                        iconoAnimado.src='animated/weather.svg'
-                          console.log('ATMOSFERA');
-                          break;  
-                      case 'Clouds':
-                          iconoAnimado.src='animated/cloudy-day-1.svg'
-                          console.log('NUBES');
-                          break;  
-                      default:
-                        iconoAnimado.src='animated/cloudy-day-1.svg'
-                        console.log('por defecto');
-                    }
-
-              })
-              .catch( error => {
-                  console.log(error)
-              })
-       })
-          
+                    case 'Clear':
+                        iconoAnimado.src = 'animated/day.svg';
+                        break;
+                    case 'Atmosphere':
+                        iconoAnimado.src = 'animated/weather.svg';
+                        break;
+                    case 'Clouds':
+                        iconoAnimado.src = 'animated/cloudy-day-1.svg';
+                        break;
+                    default:
+                        iconoAnimado.src = 'animated/cloudy-day-1.svg';
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
-})
+    function actualizar(){
+    let ciudadSeleccionada = selectDepartamento.value;
+    actualizarClima(ciudadSeleccionada);
+    }
+    
+    // Cargar el clima para la primera opción por defecto
+    actualizarClima(selectDepartamento.value);
+});
